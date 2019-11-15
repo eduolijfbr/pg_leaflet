dee0fb844f2fd9138d310e533878564f86f3c706
eduolijfbr <eduolijfbr@gmail.com>
2019-11-15 18:56:54 -0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,26 @@
-var map = L.map('map-template').setView([51.505, -0.09], 13);
+var openstreetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var osm_forest   = L.tileLayer('http://tile.thunderforest.com/landscape/{z}/{x}/{y}.png');
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+var map = L.map('map-template', {
+    center: [51.505, -0.09],
+    zoom: 10,
+    layers: [openstreetmap, osm_forest]
+});
+
+var baseMaps = {
+    "openstreetmap": openstreetmap,
+    "osm_forest": osm_forest
+};
+
+var overlayMaps = {
+    //"Cities": cities
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+//var map = L.map('map-template').setView([51.505, -0.09], 13);
+
+//L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
