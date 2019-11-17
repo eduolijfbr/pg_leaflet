1ee54d53c721e402d18bcdaa0fe62f77b767c8f2
eduolijfbr <eduolijfbr@gmail.com>
2019-11-17 07:38:35 -0300

    Add files via upload

--- pg/index.js
+++ pg/index.js
@@ -12,28 +12,43 @@ const pool = new pg.Pool({
 
 
 camadas.get('/drenagem', (request, response) => {
-	const loteid = parseInt(request.params.loteid)
-	pool.query('SELECT row_to_json(fc) FROM (SELECT \'FeatureCollection\' As type, array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, ST_AsGeoJSON(ST_Multi(st_transform(lg.geom,4326)))::json As geometry, row_to_json((lg.gid, lg.codrede)) As properties FROM drenagem As lg) As f) As fc;', (error, results) => {
+
+	var string_text = "SELECT row_to_json(fc) FROM (SELECT \'FeatureCollection\' As type, array_to_json(array_agg(f)) As features FROM (SELECT \'Feature\' As type, ST_AsGeoJSON(ST_Multi(st_transform(lg.geom,4326)))::json As geometry, row_to_json((lg.gid, lg.codrede)) As properties FROM drenagem As lg) As f) As fc;";
+	pool.query(string_text, (error, results) => {
 	    if (error) {
 	      throw error
-	    }
-	    
+	    }	    
 	    origin = results.rows;
 		origin = Object.assign({}, origin);
 		origin = origin[0];
 		origin = origin['row_to_json'];
-
 	    response.status(200).json(origin);
-	/*
-		fs.writeFile("drenagem.geojson", JSON.stringify(origin), function(err) {
-		    if(err) {
-		        return console.log(err);
-		    }
 
-		    console.log("The file was saved!");
-		});
-	*/
     })
+})
+
+camadas.get('/lotes/:nelng/:nelat/:swlng/:swlat', (request, response) => {
+
+	const nelng = request.params.nelng;
+	const nelat = request.params.nelat;
+	const swlng = request.params.swlng;
+	const swlat = request.params.swlat;
+
+	var string_text = "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(ST_Multi(st_transform(lg.geom,4326)))::json As geometry, row_to_json((lg.gid, lg.loteid	)) As properties FROM (select a.gid, a.geom, a.loteid from lotes a, (select st_transform(st_makepolygon(ed_extent_to_line ("+nelng+", "+nelat+", "+swlng+", "+swlat+", 4326)),29193) as geom) b where st_intersects(a.geom, st_transform(b.geom,29193))) As lg) As f) As fc;";
+	//console.log(string_text);
+	
+	pool.query(string_text, (error, results) => {
+	    if (error) {
+	      throw error
+	    }	    
+	    origin = results.rows;
+		origin = Object.assign({}, origin);
+		origin = origin[0];
+		origin = origin['row_to_json'];
+	    response.status(200).json(origin);
+
+    })
+    
 })
 
 module.exports = camadas;
--- public/js/main.js
+++ public/js/main.js
@@ -4,29 +4,79 @@ var osm_forest = L.tileLayer('http://tile.thunderforest.com/landscape/{z}/{x}/{y
 // criar pagina em branco.
 var white = L.tileLayer("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAAA1BMVEX///+nxBvIAAAAH0lEQVQYGe3BAQ0AAADCIPunfg43YAAAAAAAAAAA5wIhAAAB9aK9BAAAAABJRU5ErkJggg==");
 
-var drenagem = L.geoJson.ajax('drenagem');
+var lotes = new L.FeatureGroup();
+var drenagem = new L.FeatureGroup();
 
 var lat = -21.76219;
 var lng = -43.35046;
 
 var map = L.map('map-template', {
     center: [lat, lng],
-    zoom: 10,
-    layers: [openstreetmap, osm_forest, white]
+    zoom: 16,
+    layers: [openstreetmap]
 });
 
-var baseMaps = {
-    "openstreetmap": openstreetmap,
-    "osm_forest": osm_forest
+function func_drenagem(){
+	drenagem = L.geoJson.ajax('drenagem');
+	return drenagem;
+}
+
+function func_lotes(){
+	var ext = map.getBounds();
+	var nelng = ext._northEast.lng;
+	var nelat = ext._northEast.lat;
+	var swlng = ext._southWest.lng;
+	var swlat = ext._southWest.lat;
+
+	lotes = L.geoJson.ajax('lotes/'+nelng+'/'+nelat+'/'+swlng+'/'+swlat);
+	return lotes;
+	console.log("teste");
+}
+
+map.on('moveend', function() {
+	zoomLev = map.getZoom();
+	
+	if (zoomLev >= 15){
+		func_lotes();
+	}
+	if (zoomLev < 15){		
+		func_drenagem();
+	}
+	
+	console.log(zoomLev);
+})
+
+map.on('zoomend', function(){
+	var z = map.getZoom();
+	
+	if (z >= 15) {
+		lotes.addTo(map);
+	}
+	if (z < 15){
+		map.removeLayer(lotes);
+		lotes.clearLayers();
+		lotes.removeFrom(map);
+	}
+	
+});
+
+func_drenagem();
+func_lotes();
+
+var baseMaps = {    
+	"openstreetmap": openstreetmap,
+    "osm_forest": osm_forest,
+    "White": white,    
 };
 
 var overlayMaps = {
     "Drenagem": drenagem,
+    "Lotes": lotes
 };
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
-
+/*
 L.marker([lat, lng]).addTo(map)
     .bindPopup('Juiz de Fora - MG')
     .openPopup();
-
+*/
