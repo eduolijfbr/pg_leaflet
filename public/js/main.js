50dde98e34f8e1bad24c23a894e5e4609292c9b0
Eduardo de Oliveira <eduolijfbr@gmail.com>
2019-11-24 22:21:10 -0300

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,8 @@ var osm_forest = L.tileLayer('http://tile.thunderforest.com/landscape/{z}/{x}/{y
 // criar pagina em branco.
 var white = L.tileLayer("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAAA1BMVEX///+nxBvIAAAAH0lEQVQYGe3BAQ0AAADCIPunfg43YAAAAAAAAAAA5wIhAAAB9aK9BAAAAABJRU5ErkJggg==");
 
+
+
 var lotes = new L.FeatureGroup();
 var drenagem = new L.FeatureGroup();
 
@@ -33,9 +35,23 @@ function bounds(){
 
 }
 
+map.on('overlayadd overlayremove', function (e) {
+	console.log(e);
+    if (control._handlingClick) {
+        // Executes only on input toggle, not on
+        // map.addLayer(marker) or map.removeLayer(marker)
+        map.addLayer(lotes);
+    }
+});
+
 map.on('move', function() {
 	zoomLev = map.getZoom();
 	
+	//var lc = document.getElementsByClassName('leaflet-control-layers');
+	//lc[0].style.visibility = 'hidden';
+	//$('.leaflet-control-layers').hide();
+	//console.log(lc);
+
     if(zoomLev >= 16){
 		map.removeLayer(lotes);
 		lotes.clearLayers();
@@ -44,12 +60,24 @@ map.on('move', function() {
 		lotes = L.geoJson.ajax('lotes/'+text, {style: polySymbol_lotes});
 		//lotes = L.geoJson.ajax('lotes/'+nelng+'/'+nelat+'/'+swlng+'/'+swlat, {style: polySymbol_lotes});
 		lotes.addTo(map);
-		
+
 	}else{
 		map.removeLayer(lotes);
 		lotes.clearLayers();
 	}
+
 });
+
+/*
+// PARA CRIAR UM ELEMENTO HTML NO MAPA.
+var autoZoomCheckbox = L.control({position: 'topleft'});
+autoZoomCheckbox.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'command');
+    div.innerHTML = '<form><input id="command" type="checkbox"/>Automatic fit to bounds</form>';
+    return div;
+};
+autoZoomCheckbox.addTo(map);
+*/
 
 var baseMaps = {    
 	"openstreetmap": openstreetmap,
@@ -59,10 +87,10 @@ var baseMaps = {
 
 var overlayMaps = {
     "Drenagem": drenagem,
-    //"Lotes": lotes
+    "Lotes": lotes
 };
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+var control = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 polySymbol_lotes = {
 	color: "#000000",
